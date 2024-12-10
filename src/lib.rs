6d01b98837de8a00b9bab453aabf8d17250137d0
HugoPhi <hugonelson07@outlook.com>
2024-12-11 01:58:24 +0800

    Linked List

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,48 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+mod linked_list;
+
+pub use linked_list::LinkedList;
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_push_and_pop() {
+        let mut list = LinkedList::new();
+
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        assert_eq!(list.pop(), Some(3));
+        assert_eq!(list.pop(), Some(2));
+        assert_eq!(list.pop(), Some(1));
+        assert_eq!(list.pop(), None);
+    }
+
+    #[test]
+    fn test_len() {
+        let mut list = LinkedList::new();
+        assert_eq!(list.len(), 0);
+
+        list.push(1);
+        list.push(2);
+        list.push(3);
+        assert_eq!(list.len(), 3);
+
+        list.pop();
+        assert_eq!(list.len(), 2);
+    }
+
+    #[test]
+    fn test_is_empty() {
+        let mut list = LinkedList::new();
+        assert!(list.is_empty());
+
+        list.push(1);
+        assert!(!list.is_empty());
+
+        list.pop();
+        assert!(list.is_empty());
     }
 }
